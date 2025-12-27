5f07cc394558778c3c10840a50a7d340e0bb4490
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-27 21:16:14 +0530

    work with the challange-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// This is an example of the basics code of the how we create the react page from the scratch
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -5,3 +6,15 @@ root.render(
     <h1>This is how we create the page</h1>
   </div>
 );
+
+// Challange -1
+// import { createRoot } from "react-dom/client";
+// const root = createRoot(document.querySelector("#root"));
+// root.render(
+//   <div>
+//     <ul>
+//       <li>Hello</li>
+//       <li>Guys</li>
+//     </ul>
+//   </div>
+// );
