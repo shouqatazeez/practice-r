5e4310ff496ca49a47719febdc86f6840ef9fe09
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-27 23:54:49 +0530

    work with how we create the custom components and the solved the challange=2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
+// This is how we create the customcomponents and also this is the challange =2
+import React from "react";
+function MainContent() {
+  return (
+    <div>
+      <h1>React is great! </h1>
+    </div>
+  );
+}
+
 // This is an example of the basics code of the how we create the react page from the scratch
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <div>
     <h1>This is how we create the page</h1>
+    <MainContent />
   </div>
 );
 
-// Challange -1
+// Challange -1 we have to print the lists in the ulordered list
 // import { createRoot } from "react-dom/client";
 // const root = createRoot(document.querySelector("#root"));
 // root.render(
