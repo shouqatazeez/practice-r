00ceaf9d8d6d2f6441b33fe0b6683f220af3e503
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-28 20:08:55 +0530

    know the difference between the declarative and imperative

--- basics/vite-project/src/main.jsx
+++ basics/vite-project/src/main.jsx
@@ -1,7 +1,10 @@
+// Declartive code - we just write the code then the jsx will manage it
+
 // import { createRoot } from "react-dom/client";
 // const root = createRoot(document.getElementById("root"));
 // root.render(<h1>Hello Bhai</h1>);
 
+// Imperative code- we have to create each and every thing from the scratch && challange -3
 const createh1 = document.createElement("h1");
 createh1.innerText =
   "This is how we create the h1 and connect it to the parent";
