5766f067368422226ebafb8d913b5aa24937d5d5
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-28 20:05:31 +0530

    completed the challange -3

--- basics/vite-project/src/main.jsx
+++ basics/vite-project/src/main.jsx
@@ -1,3 +1,9 @@
-import { createRoot } from "react-dom/client";
-const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello Bhai</h1>);
+// import { createRoot } from "react-dom/client";
+// const root = createRoot(document.getElementById("root"));
+// root.render(<h1>Hello Bhai</h1>);
+
+const createh1 = document.createElement("h1");
+createh1.innerText =
+  "This is how we create the h1 and connect it to the parent";
+createh1.className = "header";
+document.getElementById("root").appendChild(createh1);
