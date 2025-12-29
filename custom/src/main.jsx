a61e572f8ebb5b7608477fcbe19efcb5e969f662
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-29 23:55:30 +0530

    learn about how to create a function and render it into the root

--- custom/src/main.jsx
+++ custom/src/main.jsx
@@ -1,5 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+const root = createRoot(document.getElementById("root"));
+root.render(<TemproryName />);
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+function TemproryName() {
+  return (
+    <div>
+      <h1>This is the my first function component</h1>
+      <p>Hello every one</p>
+    </div>
+  );
+}
