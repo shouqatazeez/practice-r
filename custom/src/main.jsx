84ccf37b0dee7ba4d62c63324496b4a9a5e10ace
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-30 00:05:12 +0530

    completed challange part=1

--- custom/src/main.jsx
+++ custom/src/main.jsx
@@ -1,12 +1,31 @@
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
-root.render(<TemproryName />);
+root.render(
+  <div>
+    <TemproryName />
+    <Page />
+  </div>
+);
 
 function TemproryName() {
   return (
     <div>
       <h1>This is the my first function component</h1>
       <p>Hello every one</p>
+    </div>
+  );
+}
+
+// The below example code is the challange code part-1
+
+function Page() {
+  return (
+    <div>
+      <ol>
+        <li>To become the best frontend engineer</li>
+        <li>To be very good at the code and desing</li>
+        <li>To learn new things</li>
+      </ol>
     </div>
   );
 }
