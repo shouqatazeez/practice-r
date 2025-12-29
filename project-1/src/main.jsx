d5383cb91f524dd84ebea9a2695da313ad524d34
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-29 21:23:41 +0530

    work witht the project-1

--- project-1/src/main.jsx
+++ project-1/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import reactlogo from "./assets/react.svg";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <img src={reactlogo} alt="reactlogo" />
+    <h1>Fun Facts about React</h1>
+    <ul>
+      <li>Was released in the 2013</li>
+      <li>Was orginally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>It maintained by meta</li>
+      <li>power of thousands of enterprise app, including mobile apps</li>
+    </ul>
+  </div>
+);
