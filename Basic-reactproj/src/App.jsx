44fc5ad11a432fef32ce6fa9354b1d9a123cc8c6
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-31 20:08:09 +0530

    render the app.jsx inside the main.jsx

--- Basic-reactproj/src/App.jsx
+++ Basic-reactproj/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Nav from "./Components/Nav";
+import Main from "./Components/Main";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
 };
 
 export default App;
