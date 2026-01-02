efb24075a5e33c88b9a39c3722e4e76cfacc1046
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2026-01-02 22:33:43 +0530

    added img to the nav

--- travel-journal/components/Header.jsx
+++ travel-journal/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import globe from "../src/assets/global-line.png";
 
 const Header = () => {
   return (
-    <div>
+    <header>
+      <img src={globe} alt="globe" />
       <h1>This is the header component</h1>
-    </div>
+    </header>
   );
 };
 
