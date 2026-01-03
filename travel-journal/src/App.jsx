24ac07acf106be8eec355a67d3cac5a7d55ea845
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2026-01-03 15:25:58 +0530

    render entry.jsx into app

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Entry from "../components/Entry";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Entry />
     </div>
   );
 };
